3425dd27e794e9cec757895847b3540f3f75ddcb
Gustavo Firmino de Oliveira <gustavo.firmino.oli@gmail.com>
2024-10-16 01:14:41 -0300

    Criação botões + funcionalidade  escrita

--- Calculadora.dpr
+++ Calculadora.dpr
@@ -2,7 +2,8 @@ program Calculadora;
 
 uses
   Vcl.Forms,
-  U_Calculadora in 'U_Calculadora.pas' {Form1};
+  U_Calculadora in 'U_Calculadora.pas' {Form1},
+  U_Operacoes in 'U_Operacoes.pas';
 
 {$R *.res}
 
